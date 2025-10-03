3c23aacc151777ee3c0bda64889fa5fa6b9dbbfe
S_coder <scoder0011@gmail.com>
2025-10-03 12:36:05 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -22,12 +22,12 @@ from reportlab.lib import colors
 from reportlab.pdfgen import canvas
 from PIL import Image
 import io
-from auth import register_auth_routes
+
 
 app = Flask(__name__)
 CORS(app)
 
-register_auth_routes(app)
+
 
 
 ALLOWED_EXTENSIONS = {'pdf', 'docx', 'doc', 'pptx', 'ppt', 'txt', 'jpg', 'jpeg', 'png', 'gif'}
